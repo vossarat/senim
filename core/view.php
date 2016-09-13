9e6d775dfc0d92c9e0e054b7218b38309e3e16b7
vossarat <d.tarassov@akmzdrav.kz>
2016-09-13 22:00:10 +0600

    13/09

--- core/view.php
+++ core/view.php
@@ -6,17 +6,17 @@ class view
     {
         if ($generalView) {
             extract($viewdata);
-            include 'views/'.$template.'.php';
+            include "views/$template.php";
         }
         elseif (!$_POST) {
             ob_start();
-            	include 'views/'.$template.'.php';
+            	include "views/$template.php";
             	$contentHtml = ob_get_contents();
             ob_clean();
             return $contentHtml;
         }
         else {
-			include 'views/'.$template.'.php';
+			include "views/$template.php";
 		}
     }
     
